505fbfae2351a5854d2990493a368a7d5c57eee5
wilsb8 <wilsb8@gmail.com>
2022-10-17 08:18:04 +0000

    initial

---
+++ db/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Login = require("../api/models/login");
+const bcrypt = require('bcrypt')
+
+
+
+const postLogin = async (req) => {
+  const salt = await bcrypt.genSalt(10);
+  let password = req.body.password;
+  password = await bcrypt.hash(password, salt);
+
+  const login = new Login({
+    _id: mongoose.Types.ObjectId(),
+    userid: req.body.userid,
+    password: password,
+  });
+
+  return await login.save();
+};
+
+module.exports = postLogin;
