1d7a387772427e2c788350a7555c18a53fb929b8
wilsb8 <wilsb8@gmail.com>
2022-10-17 08:08:30 +0000

    initial

---
+++ api/routes/routes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const postLogin = require("../../db/db");
+
+router.get("/", () => {
+  res.status(200).json({
+    message: "Server is up.",
+    method: req.method,
+  });
+});
+
+router.post("/login", (req, res) => {
+  postLogin(req)
+    .then((result) => {
+      res.status(200).json({
+        message: "Login saved",
+        status: 200,
+        login: {
+          userid: result.userid,
+          password: result.password,
+          metadata: {
+            hostname: req.hostname,
+            method: req.method,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Login failed",
+        status: 500,
+        error: {
+          message: err.message,
+          metadata: {
+            hostname: req.hostname,
+            method: req.method,
+          },
+        },
+      });
+    });
+});
+
+module.exports = router;
